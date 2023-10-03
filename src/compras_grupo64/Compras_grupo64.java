9e5e07f9cd1ea85fa1786785a9b657bdfca921c7
Jesy Gomez <julianycia.m@gmail.com>
2023-10-03 18:55:00 -0300

    agregando

--- src/compras_grupo64/Compras_grupo64.java
+++ src/compras_grupo64/Compras_grupo64.java
@@ -1,11 +1,16 @@
 package compras_grupo64;
 
+import java.sql.Connection;
+import proveMaxGrupo64.AccesoADatos.Conexion;
+
 
 public class Compras_grupo64 {
 
 
     public static void main(String[] args) {
         // TODO code application logic here
+                Connection con = Conexion.getConexion();
+
     }
 
 }
--- src/proveMaxGrupo64/AccesoADatos/Conexion.java
+++ src/proveMaxGrupo64/AccesoADatos/Conexion.java
@@ -1,14 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package proveMaxGrupo64.AccesoADatos;
 
-/**
- * @Jesy
- */
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 public class Conexion {
+    private static final String URL="jdbc:mariadb://localhost/";
+    private static final String DB="provemax";
+    private static final String USUARIO="root";
+    private static final String PASSWORD="";
+    private static Connection connection;
+    
+    private Conexion(){}
+    
+    public static Connection getConexion(){
+    
+        if(connection==null){
+            
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+                JOptionPane.showMessageDialog(null, "Conectado");
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los Drive"+ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la base de datos"+ex.getMessage());
+            }
+        }
+        return connection;
+    }
 
 }
